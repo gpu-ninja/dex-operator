04feb157a136447ed6943dbea5d280253db1df63
Damian Peckett <damian@pecke.tt>
2023-09-17 18:37:15 +0200

    feat: add servicemonitor and update integration tests

---
+++ tests/main_test.go
@@ -0,0 +1,110 @@
+/* SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2023 Damian Peckett <damian@pecke.tt>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main_test
+
+import (
+	"context"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func TestOperator(t *testing.T) {
+	t.Log("Creating example resources")
+
+	rootDir := os.Getenv("ROOT_DIR")
+
+	require.NoError(t, createExampleResources(filepath.Join(rootDir, "examples")))
+
+	kubeconfig := filepath.Join(clientcmd.RecommendedConfigDir, "config")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	require.NoError(t, err)
+
+	clientset, err := kubernetes.NewForConfig(config)
+	require.NoError(t, err)
+
+	t.Log("Waiting for demo-client-secret to be created")
+
+	ctx := context.Background()
+	err = wait.PollUntilContextTimeout(ctx, 5*time.Second, 5*time.Minute, true, func(ctx context.Context) (bool, error) {
+		_, err := clientset.CoreV1().Secrets("default").Get(ctx, "demo-client-secret", metav1.GetOptions{})
+		if err != nil {
+			if !apierrors.IsNotFound(err) {
+				return true, err
+			}
+
+			t.Log("Not yet ready")
+
+			return false, nil
+		}
+
+		return true, nil
+	})
+	require.NoError(t, err, "failed to wait for demo-client-secret")
+
+	t.Log("Client secret created successfully")
+
+	t.Log("Deleting dex identity provider (to test cleanup)")
+
+	dynamicClient, err := dynamic.NewForConfig(config)
+	require.NoError(t, err)
+
+	gvr := schema.GroupVersionResource{
+		Group:    "dex.gpu-ninja.com",
+		Version:  "v1alpha1",
+		Resource: "dexidentityproviders",
+	}
+
+	err = dynamicClient.Resource(gvr).Namespace("default").Delete(ctx, "demo", metav1.DeleteOptions{})
+	require.NoError(t, err)
+
+	t.Log("Waiting for demo-client-secret to be deleted")
+
+	err = wait.PollUntilContextTimeout(ctx, 5*time.Second, 5*time.Minute, true, func(ctx context.Context) (bool, error) {
+		_, err := clientset.CoreV1().Secrets("default").Get(ctx, "demo-client-secret", metav1.GetOptions{})
+		if apierrors.IsNotFound(err) {
+			return true, nil
+		} else if err != nil {
+			return true, err
+		}
+
+		t.Log("Not yet deleted")
+
+		return false, nil
+	})
+	require.NoError(t, err, "failed to wait for demo-client-secret to be deleted")
+}
+
+func createExampleResources(examplesDir string) error {
+	cmd := exec.Command("kapp", "deploy", "-y", "-a", "dex-operator-examples", "-f", examplesDir)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
